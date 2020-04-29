3fe2ae84be6240f33bd084c8ff256bf79d8ca97b
antiq <kratyukan@gmail.com>
2020-04-29 20:29:59 +0300

    get index route

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const express = require('express');
+const app = express();
+app.use(express.json()); // 'application/json' parser
+
+const port = process.PORT || 3000;
+const genres = [
+  {
+    id: 1,
+    name: 'genre1'
+  },
+  {
+    id: 2,
+    name: 'genre2'
+  },
+  {
+    id: 3,
+    name: 'genre3'
+  }
+];
+
+app.get('/api/genres', (req, res) => {
+  res.status(200).send(JSON.stringify(genres));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
